9c1d59ba0a1423316e05142f738c79ebe03285cf
sabrucha <sabri.signorelli@gmail.com>
2022-06-20 21:39:40 -0300

    Cambios

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//require('dotenv').config(); var pool = require('./moedels/bd')
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//select
+// pool.query('select * from Novedades').then(function (resultados) {
+//  console.log(resultados)
+// });
 
 
 // catch 404 and forward to error handler
@@ -40,4 +48,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
